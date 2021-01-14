73128f62e6c21b3113a1bc45d9732c5f5088f99e
mionsyun <65840867+mionsyun@users.noreply.github.com>
2021-01-14 15:22:17 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+
+
+
+var vrView;
+
+// All the scenes for the experience
+var scenes = {
+  フェニックス広場: {
+    image: 'fenix-preview.jpg',
+    //preview: 'fenix-preview.jpg',
+    hotspots: {
+      教室: {
+        pitch: 8.3,
+        yaw: -51.0,
+        radius: 0.1,
+        distance: 2
+      }
+    }
+  },
+  教室: {
+    image: 'classroom-preview.jpg',
+    //preview: 'classroom-preview.jpg',
+    hotspots: {
+      フェニックス広場: {
+        pitch: -0.9,
+        yaw: -45.7,
+        radius: 0.1,
+        distance: 2
+      }
+    }
+  },
+  Yショップ:{
+    image:'yshop.JPG',
+    },
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+function onLoad() {
+  vrView = new VRView.Player('#vrview', {
+    width: '100%',
+    height: 480,
+    image: 'blank.png',
+    is_stereo: true,
+    is_yaw_only: false,
+    is_autopan_off: false,
+  });
+  vrView.on('ready', onVRViewReady);
+  vrView.on('modechange', onModeChange);
+  vrView.on('click', onHotspotClick);
+  vrView.on('error', onVRViewError);
+  vrView.on('getposition', onGetPosition);
+  
+  
+  }
+
+
+
+function onVRViewReady(e) {
+  console.log('onVRViewReady');
+  loadScene('classroom');
+}
+
+function onModeChange(e) {
+  console.log('onModeChange', e.mode);
+}
+
+function onGetPosition(e) {
+  console.log(e);
+
+}
+
+function onHotspotClick(e) {
+  vrView.getPosition()
+  console.log('onHotspotClick', e.id);
+  if (e.id) {
+    loadScene(e.id);
+  }
+}
+
+function loadScene(id) {
+  console.log('loadScene', id);
+
+  
+  // Unhighlight carousel items
+  var carouselLinks = document.querySelectorAll('ul.carousel li a');
+  for (var i = 0; i < carouselLinks.length; i++) {
+    carouselLinks[i].classList.remove('current');
+  }
+    vrView.getPosition();
+  // Highlight current carousel item
+  document.querySelector('ul.carousel li a[href="#' + id + '"]')
+      .classList.add('current');
+
+  
+  
+  // Set the image
+  vrView.setContent({
+    image: scenes[id].image,
+    preview: scenes[id].preview,
+    is_stereo: false,
+    is_autopan_off: true
+  });
+
+  // Add all the hotspots for the scene
+  var newScene = scenes[id];
+  var sceneHotspots = Object.keys(newScene.hotspots);
+  for (var i = 0; i < sceneHotspots.length; i++) {
+    var hotspotKey = sceneHotspots[i];
+    var hotspot = newScene.hotspots[hotspotKey];
+
+    vrView.addHotspot(hotspotKey, {
+      pitch: hotspot.pitch,
+      yaw: hotspot.yaw,
+      radius: hotspot.radius,
+      distance: hotspot.distance
+    });
+  }
+}
+
+function onVRViewReady(e) {
+  console.log('onVRViewReady');
+
+  // Create the carousel links
+  var carouselItems = document.querySelectorAll('ul.carousel li a');
+  for (var i = 0; i < carouselItems.length; i++) {
+    var item = carouselItems[i];
+    item.disabled = false;
+
+    item.addEventListener('click', function(event) {
+      event.preventDefault();
+      loadScene(event.target.parentNode.getAttribute('href').substring(1));
+    });
+  }
+
+  loadScene('フェニックス広場');
+}
+
+
+
+function onVRViewError(e) {
+  console.log('Error! %s', e.message);
+}
+
+window.addEventListener('load', onLoad);
